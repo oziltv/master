c6b22a05d01ee98d9065fc42e7ad6d83842fe85c
oziltv <128941521+oziltv@users.noreply.github.com>
2023-04-25 21:50:01 +0700

    Create sport.js

---
+++ event/sport.js
@@ -0,0 +1,64 @@
+
+    
+    function sony1(){document.getElementById("tv") .innerHTML='<iframe name="djbottom" src="/p/player.html?video=https://dai.google.com/linear/hls/event/6WhVNGKTRXyu588zZv1Lkg/master.m3u8?type=rakettv&m=1" width="100%" height="450" frameborder="0" id="iframeplayer" allowfullscreen="true" webkitallowfullscreen="true" mozallowfullscreen="true" data-secrt="UT9k" security="restricted" sandbox="allow-top-navigation allow-same-origin allow-forms allow-scripts">加载中...</iframe>';
+}
+    
+        function cmore2(){document.getElementById("tv") .innerHTML='<iframe name="djbottom" src="/p/cmore2.html" width="100%" height="100%" frameborder="0" id="iframeplayer" allowfullscreen="true" webkitallowfullscreen="true" mozallowfullscreen="true" data-secrt="UT9k" security="restricted" sandbox="allow-top-navigation allow-same-origin allow-forms allow-scripts">加载中...</iframe>';
+
+}  
+   
+    
+   function sony2(){document.getElementById("tv") .innerHTML='<iframe name="djbottom" src="/p/player.html?video=https://dai.google.com/linear/hls/event/LK-ik89MQIi_pWBbg74KNQ/master.m3u8?type=rakettv&m=1" width="100%" height="450" frameborder="0" id="iframeplayer" allowfullscreen="true" webkitallowfullscreen="true" mozallowfullscreen="true" data-secrt="UT9k" security="restricted" sandbox="allow-top-navigation allow-same-origin allow-forms allow-scripts">加载中...</iframe>';
+
+}    
+    
+
+       function dazn37(){document.getElementById("tv") .innerHTML='<iframe name="djbottom" src="/p/daz-37.html" width="100%" height="450" frameborder="0" id="iframeplayer" allowfullscreen="true" webkitallowfullscreen="true" mozallowfullscreen="true" data-secrt="UT9k" security="restricted" sandbox="allow-top-navigation allow-same-origin allow-forms allow-scripts">加载中...</iframe>';
+
+}  
+    
+    
+       function tv3max(){document.getElementById("tv") .innerHTML='<iframe name="djbottom" src="/p/tv-max-3.html" width="100%" height="450" frameborder="0" id="iframeplayer" allowfullscreen="true" webkitallowfullscreen="true" mozallowfullscreen="true" data-secrt="UT9k" security="restricted" sandbox="allow-top-navigation allow-same-origin allow-forms allow-scripts">加载中...</iframe>';
+
+}
+    
+       function dazn17(){document.getElementById("tv") .innerHTML='<iframe name="djbottom" src="/p/dazn-17.html?m=1" width="100%" height="100%" frameborder="0" id="iframeplayer" allowfullscreen="true" webkitallowfullscreen="true" mozallowfullscreen="true" data-secrt="UT9k" security="restricted" sandbox="allow-top-navigation allow-same-origin allow-forms allow-scripts">加载中...</iframe>';
+
+}      
+
+      function dazn24(){document.getElementById("tv") .innerHTML='<iframe name="djbottom" src="/p/dazn-24.html?m=1" width="100%" height="100%" frameborder="0" id="iframeplayer" allowfullscreen="true" webkitallowfullscreen="true" mozallowfullscreen="true" data-secrt="UT9k" security="restricted" sandbox="allow-top-navigation allow-same-origin allow-forms allow-scripts">加载中...</iframe>';
+
+}    
+    
+     function polsat(){document.getElementById("tv") .innerHTML='<iframe name="djbottom" src="/p/polsat.html?m=1" width="100%" height="100%" frameborder="0" id="iframeplayer" allowfullscreen="true" webkitallowfullscreen="true" mozallowfullscreen="true" data-secrt="UT9k" security="restricted" sandbox="allow-top-navigation allow-same-origin allow-forms allow-scripts">加载中...</iframe>';
+
+}    
+        
+     function nba11(){document.getElementById("tv") .innerHTML='<iframe name="djbottom" src="/p/nba-league.html" width="100%" height="100%" frameborder="0" id="iframeplayer" allowfullscreen="true" webkitallowfullscreen="true" mozallowfullscreen="true" data-secrt="UT9k" security="restricted" sandbox="allow-top-navigation allow-same-origin allow-forms allow-scripts">加载中...</iframe>';
+
+}   
+    
+    
+    function nba12(){document.getElementById("tv") .innerHTML='<iframe name="djbottom" src="/p/nba12.html" width="100%" height="100%" frameborder="0" id="iframeplayer" allowfullscreen="true" webkitallowfullscreen="true" mozallowfullscreen="true" data-secrt="UT9k" security="restricted" sandbox="allow-top-navigation allow-same-origin allow-forms allow-scripts">加载中...</iframe>';
+
+}    
+    
+    
+<!--FUTSAL-->
+function fulsat(){document.getElementById("tv") .innerHTML='<iframe name="djbottom" src="/p/ct-sport-czech.html" width="100%" height="100%" frameborder="0" id="iframeplayer" allowfullscreen="true" webkitallowfullscreen="true" mozallowfullscreen="true" data-secrt="UT9k" security="restricted" sandbox="allow-top-navigation allow-same-origin allow-forms allow-scripts">加载中...</iframe>';
+
+}  
+    
+    
+ <!--Premier League-->
+function vsfootball(){document.getElementById("tv") .innerHTML='<iframe name="djbottom" src="/p/vs-football.html" width="100%" height="100%" frameborder="0" id="iframeplayer" allowfullscreen="true" webkitallowfullscreen="true" mozallowfullscreen="true" data-secrt="UT9k" security="restricted" sandbox="allow-top-navigation allow-same-origin allow-forms allow-scripts">加载中...</iframe>';
+
+}
+    
+function vsextra(){document.getElementById("tv") .innerHTML='<iframe name="djbottom" src="/p/vs-exrta.html" width="100%" height="100%" frameborder="0" id="iframeplayer" allowfullscreen="true" webkitallowfullscreen="true" mozallowfullscreen="true" data-secrt="UT9k" security="restricted" sandbox="allow-top-navigation allow-same-origin allow-forms allow-scripts">加载中...</iframe>';
+
+ function vprem(){document.getElementById("tv") .innerHTML='<iframe name="djbottom" src="/p/vprem.html" width="100%" height="100%" frameborder="0" id="iframeplayer" allowfullscreen="true" webkitallowfullscreen="true" mozallowfullscreen="true" data-secrt="UT9k" security="restricted" sandbox="allow-top-navigation allow-same-origin allow-forms allow-scripts">加载中...</iframe>';
+                           
+                           
+}  
+     
